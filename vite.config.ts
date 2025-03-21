8a0bdd0c81c11262c3be3eeec8a6839f09ce0bc7
suryakumara <suryakumara33@gmail.com>
2025-03-21 20:05:27 +0800

    remove react config

--- vite.config.ts
+++ vite.config.ts
@@ -12,13 +12,13 @@ export default defineConfig({
       fileName: () => "accessibility-widget.js",
     },
     rollupOptions: {
-      external: ["react", "react-dom"],
-      output: {
-        globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
-        },
-      },
+      // external: ["react", "react-dom"],
+      // output: {
+      //   globals: {
+      //     react: "React",
+      //     "react-dom": "ReactDOM",
+      //   },
+      // },
     },
   },
 });
