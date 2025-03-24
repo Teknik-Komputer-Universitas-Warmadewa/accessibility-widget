7264b27a28d44927ddf8ce497d2855c542da53dc
suryakumara <suryakumara33@gmail.com>
2025-03-24 10:16:28 +0800

    fix button

--- src/Menu.tsx
+++ src/Menu.tsx
@@ -10,7 +10,7 @@ const FloatingMenu = () => {
       {/* Floating Button */}
       <button
         onClick={() => setIsOpen(true)}
-        className="fixed z-10 top-36 right-5 bg-blue-600 text-white w-14 h-14 rounded-full shadow-lg flex items-center justify-center hover:bg-blue-700 transition"
+        className="fixed z-50 top-36 right-5 bg-blue-600 text-white w-14 h-14 rounded-full shadow-lg flex items-center justify-center hover:bg-blue-700 transition"
       >
         <FaUniversalAccess size={32} />
       </button>
